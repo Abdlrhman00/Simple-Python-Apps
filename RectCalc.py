3a47d53f9c753d2451c2b2c2cf4458791c0fadbb
Abdlrhman00 <133212657+Abdlrhman00@users.noreply.github.com>
2024-04-11 17:59:51 +0200

    apply some adjusting

--- RectCalc.py
+++ RectCalc.py
@@ -6,6 +6,6 @@ class Rectangle:
     def calculate_area(self):
         return self.length * self.width
 
-# Example usage:
-rectangle1 = Rectangle(5, 4)  # Creating a Rectangle object with length 5 and width 4
-print("Area of rectangle:", rectangle1.calculate_area())  # Calculating and printing the area
+
+rectangle1 = Rectangle(5, 4)  
+print("Area of rectangle:", rectangle1.calculate_area()) 
